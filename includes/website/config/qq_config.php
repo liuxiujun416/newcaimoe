6cba817fa3088391930cbd05daf23372f0402b58
liuxiujun <472480701@qq.com>
2017-12-23 22:21:54 +0800

    ee

---
+++ includes/website/config/qq_config.php
@@ -0,0 +1,8 @@
+<?php 
+ // 第三方插件登录信息---------------------
+define('RANK_ID' , '103'); 
+define('APP_KEY' , '101111657'); 
+$config['APP_KEY'] = '101111657'; 
+define('APP_SECRET' , '4832c00c24c558d11c3c989e15d3d4a1'); 
+$config['APP_SECRET'] = '4832c00c24c558d11c3c989e15d3d4a1'; 
+?>
