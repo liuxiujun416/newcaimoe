a222074cfbda857bcbe277178bdac0fb6ade7f34
liuxiujun <472480701@qq.com>
2017-12-23 22:05:03 +0800

    eeeeee

---
+++ data/config.php
@@ -0,0 +1,35 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "caimoeye";
+
+// database username
+$db_user   = "root";
+
+// database password
+$db_pass   = "root123";
+
+// table prefix
+$prefix    = "ecs_";
+
+$timezone    = "Asia/Chongqing";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin5566');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '2017-09-07 12:16:09');
+
+?>
---
+++ data/flashdata/pinkfocus/data.js
@@ -0,0 +1,13 @@
+imgUrl1="data/afficheimg/20140429vulvvd.jpg";
+imgtext1="";
+imgLink1=escape("http://www.caimoeye.com/category.php?id=91");
+imgUrl2="data/afficheimg/20140427mtxbxr.jpg";
+imgtext2="";
+imgLink2=escape("http://www.caimoeye.com/category.php?id=151");
+imgUrl3="data/afficheimg/20140504gktnlw.jpg";
+imgtext3="";
+imgLink3=escape("http://caimoeye.gotoip1.com/category.php?id=192");
+
+var pics=imgUrl1+"|"+imgUrl2+"|"+imgUrl3;
+var links=imgLink1+"|"+imgLink2+"|"+imgLink3;
+var texts=imgtext1+"|"+imgtext2+"|"+imgtext3;
