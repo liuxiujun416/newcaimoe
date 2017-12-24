9f3361594f75a37be74d7309a5ad9d60db48bf88
liuxiujun <472480701@qq.com>
2017-12-24 12:22:20 +0800

    eee

--- includes/lib_common.php
+++ includes/lib_common.php
@@ -615,7 +615,7 @@ function load_config()
     if ($data === false)
     {
         $sql = 'SELECT code, value FROM ' . $GLOBALS['ecs']->table('shop_config') . ' WHERE parent_id > 0';
-        echo $sql; exit;
+        print_r($GLOBALS['db']);exit;
         $res = $GLOBALS['db']->getAll($sql);
         print_r($res); exit;
         foreach ($res AS $row)
